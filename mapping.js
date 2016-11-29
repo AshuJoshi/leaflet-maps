76c73716cf40fb0a1e5567ff657622e442172812
Ashu Joshi <ashu@movinture.com>
2016-11-28 20:32:45 -0500

    updated tile fetch to https, had missed one

--- mapping.js
+++ mapping.js
@@ -1,4 +1,4 @@
-var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+var OpenStreetMap_BlackAndWhite = L.tileLayer('https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
